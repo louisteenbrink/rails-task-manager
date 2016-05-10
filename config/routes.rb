ed7cdc229d53a83bc933e901ff64cdafe167f75a
louis steenbrink <louis.steenbrink@gmail.com>
2016-05-10 11:45:52 +0200

    added gem better errors & binding of caller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  ressources :taks, only [:index, :show]
+
+  # read all tasks
+  get 'tasks' => 'tasks#index'
+
+  get 'tasks/:id' => 'tasks#show'
+
+
+  # ressources :taks, only [:index, :show]
 end
 
 # index
